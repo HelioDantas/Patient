8238f9e5836af9f79a4306e2f6ebdf0770774942
Hélio Dantas <helio_dantas@outlook.com>
2023-03-10 19:27:22 -0300

    test: criado teste quando encontrado no redis

--- tests/Unit/app/Service/ZipCodeServiceTest.php
+++ tests/Unit/app/Service/ZipCodeServiceTest.php
@@ -30,4 +30,22 @@ class ZipCodeServiceTest extends TestCase
         $result = $service->find('21210680');
         $this->assertEquals($mockResponse, $result);
     }
+
+    public function testFindWhenFindRedis()
+    {
+        $mockResponse = [
+            'cep' => '21210680',
+            'logradouro' => 'Rua B',
+            'bairro' => 'Bairro Penha',
+            'localidade' => 'Rio de Janeiro',
+            'uf' => 'UF',
+        ];
+
+        Redis::shouldReceive('get')->andReturn($mockResponse);
+
+        $service = new ZipCodeService();
+
+        $result = $service->find('21210680');
+        $this->assertEquals($mockResponse, $result);
+    }
 }
